23a711aedc07435c883b0252076d2a84e70e7e8c
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-10-10 15:29:22 +0600

    configure primary and secondary olors

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./route/Route.jsx";
+import router from "./routes/Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
