56e5fa9c564d1530c27e3628d277ba83f19f93d9
hbashar434 <h.bashar434@gmail.com>
2023-10-10 15:21:29 +0600

    modified tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "my-sans": ["Suisse", "sans"],
+        "my-serif": ["Zapf Humanist", "serif"],
+      },
+      backgroundColor: {
+        "my-primary": "#04ce78",
+        "my-secondary": "#005B41",
+      },
+      textColor: {
+        "my-primary": "#187776",
+        "my-secondary": "#F3FDE8",
+      },
+      darkMode: "class",
+    },
   },
+
   plugins: [],
-}
+};
