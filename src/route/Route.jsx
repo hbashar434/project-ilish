a5ea1df57b2ea27244d9964249743060bbaf923b
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-10-10 12:55:46 +0600

    basic routing setup

---
+++ src/route/Route.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Error from "../pages/Error/Error";
+import Home from "../pages/Home/Home/Home";
+import Blog from "../pages/Blog/Blog";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+    ],
+  },
+]);
+
+export default router;
