571858bbac2ed3f0288ddba246eaf0815c60a508
Doha0 <abu.jardoha@gmail.com>
2023-10-19 22:28:09 +0600

    footer added

---
+++ src/pages/Home/ShopNow/ShopNow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShopNow = () => {
+    return (
+        <section>
+            {/* This section is for asif */}
+            {/* http://preview.themeforest.net/item/fishinggear-fishing-accessories-woocommerce-theme/full_screen_preview/45630720?_ga=2.142189746.1021593575.1696911581-1358485714.1688400906 */}
+        </section>
+    );
+};
+
+export default ShopNow;
