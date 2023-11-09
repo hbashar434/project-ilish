d52e3d207c2ba553a32ebc8a296a6858f134362e
hbashar434 <h.bashar434@gmail.com>
2023-11-09 19:05:06 +0600

    initial

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseURL = "https://project-ilish-server.vercel.app";
+// const baseURL='http://localhost:5000/'
+
 const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${baseURL}/api` }),
   endpoints: () => ({}),
 });
-
 
 export default baseApi;
